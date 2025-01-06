c3037ae88f8db09de2f11c4956e912a6d2425cb3
Jaidev Kaleeswaran <kaleeswaranjaidev@gmail.com>
2025-01-05 21:35:17 -0800

    Added a sky, floor and added movement with mouse aka cursor

--- player.py
+++ player.py
@@ -32,10 +32,10 @@ class Player:
 
         self.collision(dx, dy)
 
-        if keys[pg.K_LEFT]:
-            self.angle -= PLAYER_ROT_SPEED * self.game.delta_time
-        if keys[pg.K_RIGHT]:
-            self.angle += PLAYER_ROT_SPEED * self.game.delta_time
+        # if keys[pg.K_LEFT]:
+        #     self.angle -= PLAYER_ROT_SPEED * self.game.delta_time
+        # if keys[pg.K_RIGHT]:
+        #     self.angle += PLAYER_ROT_SPEED * self.game.delta_time
 
         self.angle %= math.tau
 
@@ -43,9 +43,10 @@ class Player:
         return (x,y) not in self.game.map.world_map
     
     def collision(self,dx, dy):
-        if (self.check_wall(int(self.x + dx), int(self.y))):
+        scale = PLAYER_SIZE_SCALE / self.game.delta_time
+        if (self.check_wall(int(self.x + dx * scale), int(self.y))):
             self.x += dx
-        if (self.check_wall(int(self.x), int(self.y + dy))):
+        if (self.check_wall(int(self.x), int(self.y + dy * scale))):
             self.y += dy
 
     def draw(self):
@@ -54,9 +55,17 @@ class Player:
         #              self.y * 100 + WIDTH * math.sin(self.angle)),2)     
         pg.draw.circle(self.game.screen, 'green', (self.x * 100, self.y * 100), 15)        
 
+    def mouse_control(self):
+        mx, my = pg.mouse.get_pos()
+        if mx < MOUSE_BORDER_LEFT or mx > MOUSE_BORDER_RIGHT:
+            pg.mouse.set_pos([HALF_WIDTH, HALF_HEIGHT])
+        self.rel = pg.mouse.get_rel()[0]
+        self.rel = max(-MOUSE_MAX_REL, min(MOUSE_MAX_REL, self.rel))
+        self.angle += self.rel * MOUSE_SENS * self.game.delta_time
+
     def update(self):
         self.movement()
-
+        self.mouse_control()
     @property
     def pos(self):
         return self.x, self.y
