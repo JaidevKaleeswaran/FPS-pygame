457508f999c02a9381040bfdcc6372469e6396f9
Jaidev Kaleeswaran <kaleeswaranjaidev@gmail.com>
2025-01-05 21:11:02 -0800

    Cleaned up walls so framerate does not drop when too close

--- object_rendering.py
+++ object_rendering.py
@@ -6,9 +6,17 @@ class ObjectRenderer:
         self.game = game
         self.screen = game.screen
         self.wall_textures = self.load_wall_textures()
+    
+    def draw(self):
+        self.render_game_object()
+
+    def render_game_object(self):
+        list_objects = self.game.raycasting.objects_to_render
+        for depth, image, pos in list_objects:
+            self.screen.blit(image, pos)    
 
     @staticmethod
-    def get_texture(path, res =(TEXTURE_SIZE, TEXTURE_SIZE)):
+    def get_texture(path, res=(TEXTURE_SIZE, TEXTURE_SIZE)):
         texture = pg.image.load(path).convert_alpha()
         return pg.transform.scale(texture, res)
     
@@ -19,5 +27,4 @@ class ObjectRenderer:
             3 : self.get_texture('resources/textures/3.png'),
             4 : self.get_texture('resources/textures/4.png'),
             5 : self.get_texture('resources/textures/5.png'),
-
         }
